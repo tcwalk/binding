bda68673542bafa254576e0e2d6ead5dcd54c54c
Tom Walk <tcwalk@gmail.com>
2021-02-28 09:45:33 -0600

    Start tkinter GUI and main method

---
+++ getInput.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from tkinter import *
+import tkinter as tk
+from tkinter.filedialog import askopenfile
+
+
+
+def start():
+    print("start")
+
+
+
+
+if __name__ == "__main__":
+    start()
+
+
