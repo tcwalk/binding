f528948b84c332a9a7cdf67098466088989228dd
Tom Walk <tcwalk@gmail.com>
2021-02-28 14:13:58 -0600

    Added Start Analysis buttong and count_binding function. Changed Tkinter layout from grid to pack

--- getInput.py
+++ getInput.py
@@ -4,6 +4,7 @@ from tkinter import *
 from tkinter import font
 import tkinter as tk
 from tkinter.filedialog import askopenfile
+import sys
 
 source = "No file selected"
 
@@ -14,6 +15,18 @@ def choose_file(label):
     source = askopenfile(mode='r', filetypes=[('tab separated files', '*.tsv')])
 #    print(source)
     label.config(text=source.name)
+
+
+def count_binding(infile):
+    ### call global variables
+#    global source
+    ### get file from user and update source label
+#    source = askopenfile(mode='r', filetypes=[('tab separated files', '*.tsv')])
+    print("counting")
+    print(source.name)
+
+#    label.config(text=source.name)
+
 
 
 ### Main calls start. The app starts here
@@ -35,22 +48,29 @@ def start():
     root["bg"] = background
 
     intro_frame = Frame(root, bd=7, bg=background)
-    intro_frame.grid(row=1, column=1)
+    intro_frame.pack()
     intro_label = Label(intro_frame, bg=background, width=60, anchor="w",
                     text="Choose tab separated file listing protein binding")
-    intro_label.grid(row=1, column=1)
+    intro_label.pack()
     intro_label["anchor"] = "center"
 
     selected_frame = Frame(root, bd=7, bg=background)
-    selected_frame.grid(row=3, column=1)
+    selected_frame.pack()
     selected_label = Label(selected_frame, bg=background, text=source)
     selected_label.grid(row=3, column=2)
 
     ### Select file here. Use lambda to pass label for updating as argument
     src_frame = Frame(root, bd=7, bg=background)
-    src_frame.grid(row=2, column=1)
+    src_frame.pack()
     src_file_button = Button(src_frame, bg=button_background, text='CHOOSE FILE', command=lambda:choose_file(selected_label))
-    src_file_button.grid(row=2, column=4)
+    src_file_button.pack(side="top", pady=10)
+
+    start_btn = Button(src_frame, text='Run Analysis', command=lambda: count_binding(source.name))
+    start_btn.pack(side="left", padx=10, pady=10)
+    close_btn = Button(src_frame, bg=button_background, text='Exit', command=lambda: sys.exit())
+    close_btn.pack(side="right", padx=10, pady=10)
+
+
 
     root.mainloop()
 
